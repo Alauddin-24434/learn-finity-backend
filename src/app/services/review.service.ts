878f3495a9c4fdb73a98d687ed7a659cc8c30a9f
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2025-11-25 09:30:40 +0600

    revie ref

--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -3,9 +3,7 @@ import { IReview } from "../interfaces/review.interface";
 import { prisma } from "../lib/prisma";
 
 const createReview = async (payload: IReview) => {
-  
-  
-    const create = await prisma.review.create({
+  const create = await prisma.review.create({
     data: {
       courseId: payload.courseId,
       userId: payload.userId,
@@ -13,24 +11,34 @@ const createReview = async (payload: IReview) => {
       comment: payload.comment,
       isDeleted: false,
     },
-    });
-    
-    return create;
+  });
+
+  return create;
 };
 
-const getReviewByCourseId = async (courseId:string,) => {
-    const result = await prisma.review.findMany({
-        where: { courseId },
-        select:{userId: true}
-    })
-    if (!result) {
-        throw new AppError(404, "Review Not Found")
-    }
+const getReviewByCourseId = async (courseId: string) => {
+  const result = await prisma.review.findMany({
+    where: { courseId },
+    include: {
+      user: {
+        // <-- user extract
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  if (!result) {
+    throw new AppError(404, "Review Not Found");
+  }
 
-    return result;
-}
+  return result;
+};
 
 export const reviewSrvices = {
-    createReview,
-    getReviewByCourseId
+  createReview,
+  getReviewByCourseId,
 };
